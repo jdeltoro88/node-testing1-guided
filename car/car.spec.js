3798919182916e8d046300638dd5667bba611713
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-24 17:30:16 +0100

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,6 +1,6 @@
 // unit tests test the smallest parts of a program
 // functions, classes or modules with functions
-// 
+// external libraries are mocked
 
 describe('intro to jest', () => { // to organize
   test('returns a new value', () => { // test
