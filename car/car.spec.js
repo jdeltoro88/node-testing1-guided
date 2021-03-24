0cc0175b783d767a5f2963342d3c160a66fb3c14
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-24 18:15:43 +0100

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -19,6 +19,7 @@ describe('Car class', () => {
   it('cars can be initialized with a make and model', () => {
     // we can instantiate a car passing make and model
     // and that gives us an object with same make and model
+    const focus 
   })
 })
 
