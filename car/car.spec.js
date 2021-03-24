d79aeb6a5ff99fda4291f7b33cd54bf45a762936
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-24 18:00:00 +0100

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -9,8 +9,9 @@ describe('Car class', () => {
   })
   it('we can make a car with it', () => {
     const prius = new Car()
-    expect(prius).
+    expect(prius).toBeInstanceOf(Car)
   })
+  it('')
 })
 
 function addsOne(num) {
